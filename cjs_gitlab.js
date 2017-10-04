68277a7195ff0e20a08e95549d48655aafe1930d
Max Levitsky <max.levicky@gmail.com>
2017-10-04 16:18:03 +0200

    Create cjs_gitlab.js

---
+++ cjs_gitlab.js
@@ -0,0 +1,27 @@
+defaultBranch();
+window.onclick = function() {
+    setTimeout(
+        function(){
+            defaultBranch();
+            
+        }, 1000);
+}
+function defaultBranch() {
+    var path = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1);
+    if (path === 'branches' && !findGetParameter('sort')) {
+        window.location.href = window.location.href + "?sort=recently_updated"
+    }
+}
+
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+    .substr(1)
+        .split("&")
+        .forEach(function (item) {
+        tmp = item.split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    });
+    return result;
+}
